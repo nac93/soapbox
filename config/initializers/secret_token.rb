bece1d0901ea3f75f5ef5c1fbd04693a5d782659
Nick Cheney <nac93@cornell.edu>
2012-12-13 20:35:30 -0500

    intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SoapBox::Application.config.secret_token = '824efd8b4aad5cf72e3ba7dbb32178f0a8da37b201ec5dae9d1780023c3c9fb6d46c31ebef0a80ff478fa5f68687c36f68266756511caab4854b90d7dddc090c'
